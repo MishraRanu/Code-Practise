79a2f7b4f433080e734aeea711726fbc67b7f6cb
mishraranu <ranummishra29@gmail.com>
2023-07-18 08:44:34 +0100

    Practise 2

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String fName = "Aishwarya Atmaram Mapari";
+        String fName1 = "Ranu Manoj Mishra";
+
+        Console.WriteLine("My Name is:,{0}" ,fName1, fName);
+
+    }
+}
+
